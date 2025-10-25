0882f77a2aea0603f96ccfb0fecbede65d28259f
Fooaram <choudharyf873@gmail.com>
2025-10-25 18:47:35 +0530

    some changes are done

--- script.js
+++ script.js
@@ -4,21 +4,17 @@ const historyDiv=document.querySelector('#hisDiv');
 const modeBtn=document.querySelector('#mode');
 let currMode = "light";
 
+
 function getVal(val){
-   if(inputValue.value=='0'){
-    inputValue.value=val;
-   }
-   else{
-    inputValue.value+=val;
-   }
+    
+    if(inputValue.value === '0' && !isNaN(val) && val !== '.') {
+        inputValue.value = val;
+    }
+    else{
+        inputValue.value += val;
+    }
 }
-function solve(){
-    var equation=inputValue.value;
-    subValue.value = equation+ '=';
-    var ans=eval(equation);
-    inputValue.value=ans;
-    showHistory(equation,ans);
-}
+
 function clearAll(){
     subValue.value="";
     inputValue.value='0';
@@ -36,16 +32,17 @@ function C(){
   inputValue.value="0";
 }
 
-modeBtn.onclick = () => {
-    if(currMode === 'light'){
-        currMode = "dark";
-        document.querySelector("body").style.backgroundColor = "black";
-    }
-    else{
-        currMode = "light";
-        document.querySelector("body").style.backgroundColor = "white";
-    }
-}
+// modeBtn.onclick = () => {
+//     if(currMode === 'light'){
+//         currMode = "dark";
+//         document.querySelector("body").style.backgroundColor = "black";
+       
+//     }
+//     else{
+//         currMode = "light";
+//         document.querySelector("body").style.backgroundColor = "white";
+//     }
+// }
 function removeLastDigit(){
     if(inputValue.value.length > 1){
          inputValue.value = inputValue.value.slice(0,-1);
@@ -57,8 +54,40 @@ function removeLastDigit(){
 
 }
 
+
+
 function modulus(){
-  
+    var equation = inputValue.value;
+    subValue.value = equation + '%'; 
+
+    try {
+        
+        var result = eval(equation + "/100");
+        inputValue.value = result;
+        showHistory(equation + "%", result); 
+    } catch (err) {
+        inputValue.value = "Error";
+    }
+}
+function toggleHistory(){
+    const hisSection = document.querySelector(".History");
+    if (hisSection.style.display === "none" || hisSection.style.display === "") {
+        hisSection.style.display = "block";
+    } else {
+        hisSection.style.display = "none";
+    }
+}
+
+function solve(){
+    try {
+        var equation = inputValue.value;
+        subValue.value = equation + '=';
+        var ans = eval(equation);
+        inputValue.value = ans;
+        showHistory(equation, ans);
+    } catch (error) {
+        inputValue.value = "Error";
+    }
 }
 
 
